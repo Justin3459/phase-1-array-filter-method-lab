149791701546a85f3007e3dab33da13752aae7fa
justin becker <justin3459@hotmail.com>
2023-04-05 13:17:12 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching (array, string) {
+    return array.filter(
+        function (matchingDrivers) {
+            return matchingDrivers.toUpperCase() === string.toUpperCase()
+    })
+}
+
+function fuzzyMatch (array,string){
+    return array.filter(
+        function (letter){
+            return letter.startsWith(string)
+    })
+}
+
+function matchName(array, name){
+    return array.filter(
+        function (driver) {
+            return driver.name === name
+        }
+        )
+}
